73f0732484f97d0e1dae57ec3aef2552a168d987
Jay-SCM <John.Farmer1086@gmail.com>
2023-10-23 00:17:47 +0930

    to many lines, made less lines

--- Script.js
+++ Script.js
@@ -14,27 +14,27 @@
 // Pizza is ready
 
 function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));}
+    return new Promise(resolve => setTimeout(resolve, ms));} // Return a promise that resolves after ms milliseconds
 async function startPizzaProcess() {
-    while (true) {
-        await prepareAndServePizza();}}
+    while (true) {// infinity loops because why not
+        await prepareAndServePizza();}} // Call the function to start the process loop infinity
 async function resetCard() {
-    const messageElement = document.getElementById('message');
-    const imageElement = document.getElementById('image');
-    messageElement.innerText = '';
-    imageElement.src = '';}
-async function showMessage(message, image) {
-    document.getElementById('message').innerText = message;
-    document.getElementById('image').src = image;
+    const messageElement = document.getElementById('message'); // Get the message element
+    const imageElement = document.getElementById('image'); // Get the image element
+    messageElement.innerText = ''; // Clear the message
+    imageElement.src = '';} // Clear the image
+async function showMessage(message, image) { // Function declaration with async to await
+    document.getElementById('message').innerText = message; // Set the message
+    document.getElementById('image').src = image; // Set the image source
     await delay(2000); // Display message and image for 2 seconds
-    document.getElementById('message').innerText = '';
-    document.getElementById('image').src = '';}
+    document.getElementById('message').innerText = '';  // Clear the message
+    document.getElementById('image').src = '';} // Clear the  image
 async function preparePizza() {
     await showMessage("Started preparing pizza...", 'PBASE.png');
     await delay(1000);}
 async function makeBase() {
     await showMessage("Made the base", 'base.png');
-    await delay(1500);}
+    await delay(1500);} // Wait for 1500 milliseconds
 async function addSauceAndCheese() {
     await showMessage("Added the sauce and cheese", 'baseCheese.png');
     await delay(2000);}
@@ -46,16 +46,16 @@ async function cookPizza() {
     await delay(3000);}
 function servePizza() {
     showMessage("Pizza is ready", 'pizza.png');}
-async function prepareAndServePizza() {
+async function prepareAndServePizza() { // Function declaration with async to await
     await preparePizza();
-    await resetCard();
+    await resetCard(); // Wait for each function to complete before moving to the next
     await makeBase();
     await addSauceAndCheese();
     await addToppings();
     await cookPizza();
     servePizza();
     await delay(2000);}
-    startPizzaProcess();
+    startPizzaProcess(); // Call the function to start the process
 
 /////////////////////////////////////////////////////////////////////////
 //////  TASK 4 ORGINAL CODE WITHOUT IMG CARD AND ANIMATIONS /////////////
